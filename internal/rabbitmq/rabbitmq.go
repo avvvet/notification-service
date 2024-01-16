19a6a207219a1f4d127785caa9a67f3c78ad327d
Awet Tsegazeab <avvvet@gmail.com>
2024-01-16 06:17:57 -0500

    rabbitmq publish logic

--- internal/rabbitmq/rabbitmq.go
+++ internal/rabbitmq/rabbitmq.go
@@ -1,14 +1,19 @@
 package rabbitmq
 
-import "github.com/rabbitmq/amqp091-go"
+import (
+	"context"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 type RabbitMQ struct {
-	conn    *amqp091.Connection
-	channel *amqp091.Channel
+	conn    *amqp.Connection
+	channel *amqp.Channel
 }
 
 func NewRabbitMQ(amqpURL string) (*RabbitMQ, error) {
-	conn, err := amqp091.Dial(amqpURL)
+	conn, err := amqp.Dial(amqpURL)
 	if err != nil {
 		return nil, err
 	}
@@ -22,4 +27,23 @@ func NewRabbitMQ(amqpURL string) (*RabbitMQ, error) {
 		conn:    conn,
 		channel: channel,
 	}, nil
+}
+
+func (r *RabbitMQ) Publish(queueName string, message []byte) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err := r.channel.PublishWithContext(
+		ctx,
+		"",        //exchange
+		queueName, // routing key (queue name)
+		false,     // mandatory
+		false,     // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        message,
+		},
+	)
+
+	return err
 }
